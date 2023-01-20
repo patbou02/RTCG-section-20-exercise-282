cb6f7f184dbd9ecf6d373409226dd63191330e86
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-01-20 00:49:48 -0800

    created route definitions for each of the dummy page components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,23 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import Events from './pages/Events';
+import EventDetailsPage from './pages/EventDetails';
+import NewEventPage from './pages/NewEvent';
+import EditEventPage from './pages/EditEvent';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/events', element: <Events /> },
+  { path: '/events/:id', element: <EventDetailsPage /> },
+  { path: '/events/new', element: <NewEventPage /> },
+  { path: '/events/:id/edit', element: <EditEventPage /> },
+])
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
