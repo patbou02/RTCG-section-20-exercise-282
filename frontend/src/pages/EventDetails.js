8dbf607e52497b9d7c0347e36243e0f201265917
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-01-20 02:02:26 -0800

    In events details, replaced the page's content with content available via the params when using useParams. In event navigation, replaced <a> tags with NavLinks.

--- frontend/src/pages/EventDetails.js
+++ frontend/src/pages/EventDetails.js
@@ -1,6 +1,15 @@
+import { useParams, Link } from 'react-router-dom';
+
 const EventDetailsPage = () => {
+
+  const params = useParams();
+
   return (
-    <h1>EventDetailsPage</h1>
+    <>
+      <h1>Details Page</h1>
+      <h2>Event: {params.id}</h2>
+      <Link to='..' end>Back</Link>
+    </>
   );
 };
 
