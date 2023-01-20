ae9700400b30b2af3ccceb81a779f4b3db041520
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-01-20 00:58:58 -0800

    Created new Root layout component, added it to the root router and used the Outlet to output all other children routes while at the same time inserting the MainNavigation above them.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,13 +26,20 @@ import Events from './pages/Events';
 import EventDetailsPage from './pages/EventDetails';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/events', element: <Events /> },
-  { path: '/events/:id', element: <EventDetailsPage /> },
-  { path: '/events/new', element: <NewEventPage /> },
-  { path: '/events/:id/edit', element: <EditEventPage /> },
+  {
+    page: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/events', element: <Events /> },
+      { path: '/events/:id', element: <EventDetailsPage /> },
+      { path: '/events/new', element: <NewEventPage /> },
+      { path: '/events/:id/edit', element: <EditEventPage /> },
+    ],
+  },
 ])
 
 function App() {
