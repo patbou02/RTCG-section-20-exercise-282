87c50d80705d93bfc9af011d839960a3ad0dd7b9
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-01-20 01:58:13 -0800

    replaced hard coded list items with hard coded values with some dummy data coming from an array on the page.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,14 +1,29 @@
 import { Link } from 'react-router-dom';
 
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'event 1',
+  },
+  {
+    id: 'e2',
+    title: 'event 2',
+  },
+  {
+    id: 'e3',
+    title: 'event 3',
+  },
+  {
+    id: 'e4',
+    title: 'event 4',
+  },]
+
 const EventsPage = () => {
   return (
     <>
       <h1>Events Page</h1>
       <ul>
-        <li><Link to='/events/ev1'>Event 1</Link></li>
-        <li><Link to='/events/ev2'>Event 2</Link></li>
-        <li><Link to='/events/ev3'>Event 3</Link></li>
-        <li><Link to='/events/ev4'>Event 4</Link></li>
+        {DUMMY_EVENTS.map(event => <li key={event.id}><Link to={event.id}>{event.title}</Link></li>)}
       </ul>
     </>
   );
